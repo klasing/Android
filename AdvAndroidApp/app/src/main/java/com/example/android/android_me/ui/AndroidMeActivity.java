dc31d6b6716b7db2eeacf109255dda8b32591b9e
klasing <klasing1959@gmail.com>
2017-12-21 17:18:44 +0100

    21-12-2017 17:18

--- AdvAndroidApp/app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
+++ AdvAndroidApp/app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
@@ -1,14 +1,41 @@
 package com.example.android.android_me.ui;
 
+import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.android.android_me.R;
+import com.example.android.android_me.data.AndroidImageAssets;
 
 public class AndroidMeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_android_me);
+
+        if (savedInstanceState == null) {
+            BodyPartFragment headFragment = new BodyPartFragment();
+
+            headFragment.setImageIds((AndroidImageAssets.getHeads()));
+            headFragment.setListIndex(1);
+
+            FragmentManager fragmentManager = getSupportFragmentManager();
+
+            fragmentManager.beginTransaction()
+                    .add(R.id.head_container, headFragment)
+                    .commit();
+
+            BodyPartFragment bodyFragment = new BodyPartFragment();
+            bodyFragment.setImageIds(AndroidImageAssets.getBodies());
+            fragmentManager.beginTransaction()
+                    .add(R.id.body_container, bodyFragment)
+                    .commit();
+
+            BodyPartFragment legFragment = new BodyPartFragment();
+            legFragment.setImageIds(AndroidImageAssets.getLegs());
+            fragmentManager.beginTransaction()
+                    .add(R.id.leg_container, legFragment)
+                    .commit();
+        }
     }
 }
