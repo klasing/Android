535e0888e3d8843f4b140e27a0221483d181c592
klasing <klasing1959@gmail.com>
2017-12-22 12:04:55 +0100

    22-12-2017 12:03 final

--- AdvAndroidApp/app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
+++ AdvAndroidApp/app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
@@ -17,7 +17,9 @@ public class AndroidMeActivity extends AppCompatActivity {
             BodyPartFragment headFragment = new BodyPartFragment();
 
             headFragment.setImageIds((AndroidImageAssets.getHeads()));
-            headFragment.setListIndex(1);
+
+            int headIndex = getIntent().getIntExtra("headIndex", 0);
+            headFragment.setListIndex(headIndex);
 
             FragmentManager fragmentManager = getSupportFragmentManager();
 
@@ -27,12 +29,18 @@ public class AndroidMeActivity extends AppCompatActivity {
 
             BodyPartFragment bodyFragment = new BodyPartFragment();
             bodyFragment.setImageIds(AndroidImageAssets.getBodies());
+            int bodyIndex = getIntent().getIntExtra("bodyIndex", 0);
+            bodyFragment.setListIndex(bodyIndex);
+
             fragmentManager.beginTransaction()
                     .add(R.id.body_container, bodyFragment)
                     .commit();
 
             BodyPartFragment legFragment = new BodyPartFragment();
             legFragment.setImageIds(AndroidImageAssets.getLegs());
+            int legIndex = getIntent().getIntExtra("legIndex", 0);
+            legFragment.setListIndex(legIndex);
+
             fragmentManager.beginTransaction()
                     .add(R.id.leg_container, legFragment)
                     .commit();
